60aeaa77600e52aa619f7a651222ad10221ccca9
githubFredrickAritonang <erickaritonang93@gmail.com>
2024-09-04 21:27:10 +0700

    Sudah siap

--- T01.java
+++ T01.java
@@ -1,3 +1,21 @@
-// NIM - NAMA
-// NIM - NAMA
+// 12S24001 - Fredrick Aritonang
+// 12S24001 - Fredrick Aritonang
+
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String umur;
+
+        umur = input.nextLine();
+        System.out.println(18);
+        String alamat;
+
+        alamat = input.nextLine();
+        System.out.println("Balige");
+    }
+}
 
